de6fc8ea6f67912f7c30f877272f776e2ae0d406
Victor Santarossa <vitorsantarossa@hotmail.com>
2024-08-24 08:38:27 -0300

    modified:   Aula01/Primeiro.py

--- Aula01/Primeiro.py
+++
@@ -1,8 +0,0 @@
-ContarAte = 1
-while ContarAte > 0:
-    ContarAte = int(input("Deseja contar ate: (0 parar sair):"))
-    # if ContarAte == 0:
-    #     break
-    for contador in range(ContarAte):
-        print(contador+1)
-print("Encerrado!")
