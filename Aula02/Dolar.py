12d97ddf7d408a03ce681bf25c8fc57a506e124a
Victor Santarossa <vitorsantarossa@hotmail.com>
2024-08-24 11:29:48 -0300

    new file:   Aula02/Dolar.py

---
+++ Aula02/Dolar.py
@@ -0,0 +1,11 @@
+#Montar um sistema que converta Dolar ou Euro para Real:
+
+#Converter Dolar->Real = Multiplica o preço em Dolar pela cotação do Dolar
+#Converter Euro->Real = Multiplica o preço em Euro pela cotação do Euro
+
+#Pedir:
+#- O valor do produto em Dolar ou Euro
+#- A Cotação do Dolar ou do Euro
+#- Calcular o valor em Real
+#- Mostrar o valor calculado na tela
+
