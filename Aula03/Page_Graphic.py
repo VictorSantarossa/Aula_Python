e3ad24ad6ff7947427f0154aa4a3ba69019d83b7
leticia <vitorsantarossa@hotmail.com>
2024-08-31 10:02:56 -0300

    Update Page_Graphic.py

--- Aula03/Page_Graphic.py
+++ Aula03/Page_Graphic.py
@@ -1,17 +1,23 @@
 # O "as" em frente ao import é usado para apelidar a biblioteca.
 import tkinter as tk
+from tkinter import messagebox
+
+def click():
+    messagebox.showinfo("Hello World",f"Bom dia, {entryNome.get()}")
 
 janela = tk.Tk()
-janela.geometry("800x600")
+janela.geometry("400x300")
 janela.title("Aula03_Python")
 
 labelNome = tk.Label(janela,text = "Digite seu nome:")
-labelNome.pack(padx = 10, pady = 20)
+labelNome.pack(ipadx = 10, pady = 20)
 
-entryNome = tk.Entry(janela, width=50)
-entryNome.pack(padx= 10, pady= 5)
+entryNome = tk.Entry(janela)
+entryNome.pack(ipadx= 40, pady= 5)
 
-buttonHello = tk.Button(janela, text="Clique Aqui")
-buttonHello.pack(padx= 10,pady= 30)
+button_First = tk.Button(janela, text="Clique Aqui", command = click)
+button_First.pack(ipadx= 50, pady= 5)
+
+
 
 janela.mainloop()
