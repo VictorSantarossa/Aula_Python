6f7361603be0389228230832cac27b2d0d58bb2e
leticia <vitorsantarossa@hotmail.com>
2024-08-31 11:35:49 -0300

    modified:   Aula03/Cadastro.py

--- Aula03/Cadastro.py
+++ Aula03/Cadastro.py
@@ -8,22 +8,22 @@ janela = tk.Tk()
 janela.geometry("800x600")
 janela.title("Cadastro Cliente")
 
-labelNome = tk.Label(janela,text = "Digite seu nome:")
+labelNome = tk.Label(janela,text = "Digite seu nome:", font=("Arial", 16, "bold"), fg="gray", bg="black")
 labelNome.pack(ipadx = 10, pady = (20,2))
 entryNome = tk.Entry(janela)
 entryNome.pack(ipadx= 100, pady= 5)
 
-labelEndereco = tk.Label(janela,text = "Digite seu Endereço:")
+labelEndereco = tk.Label(janela,text = "Digite seu Endereço:", font=("Arial", 16, "bold"), fg="gray", bg="black")
 labelEndereco.pack(ipadx = 10, pady = (40,2))
 entryEndereco = tk.Entry(janela)
 entryEndereco.pack(ipadx= 100, pady= 5)
 
-labelTelefone = tk.Label(janela,text = "Digite seu Telefone:")
+labelTelefone = tk.Label(janela,text = "Digite seu Telefone:", font=("Arial", 16, "bold"), fg="gray", bg="black")
 labelTelefone.pack(ipadx = 10, pady = (40,2))
 entryTelefone = tk.Entry(janela)
 entryTelefone.pack(ipadx= 100, pady= 5)
 
-button_First = tk.Button(janela, text="Gravar", command = click)
+button_First = tk.Button(janela, text="Gravar", command = click, font=("Arial", 16, "bold"), fg="black")
 button_First.pack(ipadx= 50, pady= 20)
 
 janela.mainloop()
