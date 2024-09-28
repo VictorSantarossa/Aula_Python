74c893367bc661c4fca415742342ec0285fcc4dd
Victor Santarossa <115811824+VictorSantarossa@users.noreply.github.com>
2024-09-28 09:36:16 -0300

    new file:   Aula06/Exercicio.py 	modified:   Aula06/Lista.py

---
+++ Aula06/Exercicio.py
@@ -0,0 +1,5 @@
+#Montar um sistema utilizando print e input que peça para digitar o nome de pessoas,
+#uma de cada vez, e adicioná-los em um lista chamada listaConvidados.
+#Quando o usuário apertar o enter sem ter digitado um nome, o sistema irá listar
+#os nomes que forem cadastrados.
+
---
+++ Aula06/Lista.py
@@ -0,0 +1,24 @@
+listaCompras = ['arroz','feijao','macarrao','ovo']
+print(listaCompras)
+
+listaCompras[1] = 'milho'
+print(listaCompras)
+
+listaCompras.append('batata') #O .APPEND() Adiciona item no final da lista
+print(listaCompras)
+
+listaCompras.insert(1, 'jilo') #O .INSERT() Adiciona item em uma posição
+print(listaCompras)
+
+listaCompras.sort() #O .SORT() Ordena a lista em ordem alfabetica
+print(listaCompras)
+
+listaCompras.reverse() #O .REVERSE() Inverte a ordem dos itens
+print(listaCompras)
+
+listaCompras.remove('ovo') #O .REMOVE() Ele remove o item da lista pelo nome
+print(listaCompras)
+
+listaCompras.pop(3) #O .POP() Ele remove o item da lista pela posição
+print(listaCompras)
+
