8976f472b0e765baecb0ca1b055eccba112b5407
VictorSantarossa <vitorsantarossa@hotmail.com>
2024-09-30 16:06:43 -0300

    modified:   Aula06/Clientes.py

--- Aula06/Clientes.py
+++ Aula06/Clientes.py
@@ -1,5 +1,3 @@
-
-
 print('Cadastro de clientes')
 print('Desenvolvido por Victor Santarossa')
 print('Versão 1.0')
@@ -11,8 +9,16 @@ def formataData(data):
     novaData += data[4] + data[5] + data[6] + data[7]
     return novaData
 
+def formataCPF(cpf):
+    novoCPF  = cpf[0] + cpf[1] + cpf[2] + '.'
+    novoCPF += cpf[3] + cpf[4] + cpf[5] + '.'
+    novoCPF += cpf[6] + cpf[7] + cpf[8] + '-'
+    novoCPF += cpf[9] + cpf[10]
+    return novoCPF
+
 nome =       input('Nome do Cliente : ')
 cpf =        input('CPF             : ')
+cpf =        formataCPF(cpf)
 nascimento = input('Nascimento      : ')
 nascimento = formataData(nascimento)
 logradouro = input('Logradouro      : ')
@@ -32,6 +38,11 @@ cliente = {
     'estado'     : estado
 }
 
-print(cliente.items()  )
-print(cliente.keys()   )
-print(cliente.values() )
+# Mostra os itens do dicionário cliente
+print(cliente.items())
+
+# Mostra as chaves do dicionário cliente
+print(cliente.keys())
+
+# Mostra os valores do dicionário cliente
+print(cliente.values())
